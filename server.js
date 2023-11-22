5253fd0206ed54ebf03b049b8c0f793778a45310
Bruno Tenório <tenoriobn@gmail.com>
2023-11-22 19:17:21 -0300

    Iniciando com Express

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-import http from "http";
+// import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-  "/": "Curso de Express API",
-  "/livros": "Entrei na rota livros",
-  "/autores": "Entrei na rota autores"
-};
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain"});
-  res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("Servidor escutando!");
 });
