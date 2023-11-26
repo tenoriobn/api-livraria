df1b329e511833a0f7800b3a06865c7b9476414a
Bruno Tenório <tenoriobn@gmail.com>
2023-11-26 08:51:55 -0300

    Deletando livros

--- src/app.js
+++ src/app.js
@@ -44,4 +44,10 @@ app.put("/livros/:id", (req, res) => {
   res.status(200).json(livros);
 });
 
+app.delete("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros.splice(index, 1);
+  res.status(200).send("livro removido com sucesso");
+});
+
 export default app;
